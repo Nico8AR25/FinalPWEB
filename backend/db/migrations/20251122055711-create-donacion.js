3cdcede9f63f27736de606154cb7e11a49e5e551
Rodrigo <20220632@aloe.ulima.edu.pe>
2025-11-22 01:01:11 -0500

    fix!: formateo de folders y archivos

---
+++ backend/db/migrations/20251122055711-create-donacion.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('donacions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      streamerId: {
+        type: Sequelize.INTEGER
+      },
+      streamId: {
+        type: Sequelize.INTEGER
+      },
+      monto: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('donacions');
+  }
+};
